1559030b7e87ea892d8428e82e9a50b381f1f32d
P.Diddy <omer.sh10@gmail.com>
2020-06-13 13:54:24 +0300

    Initial repository commit.
    Current files:
    1. mangal_parser: generates network excel files using mangal API.
    2. taxonomy_resolution: returns a species/genus taxonomy info using GBIF API.
    
    Tasks:
    1. Find a method to detect and remove intra and inter database duplicate networks. Consider:
    	- trusting the database and don't perform intra database checks for duplicates.
    	- hashing all networks according to a sorted list of vertex pair interactions (using the excel files) in order to detect duplicates.
    	  Note that this method assumes complete identity of networks, both in species naming and interaction weights.
    
    2. Coerce all networks to the selected IWDB format (plants as columns, pollinators as rows, indexed by id, species and genus).
       For WoL this requires simple transposition and expansion, but for Mangal networks this requires distinction between plant and pollinator species.
       That distinction can't be resolved by GBIF alone due to naming problems, and the bipartite nature of the network must be exploited.
    
    3. Find additional databases/networks from papers.

---
+++ mangal_parser.py
@@ -0,0 +1,113 @@
+import requests as req
+import openpyxl
+
+from taxonomy_resolution import get_kingdom
+
+BY_ID_URL = "https://mangal.io/api/v2/{data_type}/{value}"
+QUERY_URL = "https://mangal.io/api/v2/{data_type}?{query_by}={value}"
+SAVE_PATH = "C:\\Personal\\University\Lab\\Mangal\\{filename}.xlsx"
+
+
+def mangal_request(data_type, query_parameter, request_value, is_query=True):
+    if is_query:
+        request = QUERY_URL.format(data_type=data_type, query_by=query_parameter, value=request_value)
+    else:
+        request = BY_ID_URL.format(data_type=data_type, value=request_value)
+    response = req.get(request)
+
+    if response.status_code != 200:
+        raise ConnectionError("request '{0}' failed with error code {1}.".format(request, response.status_code))
+    return response.json()
+
+
+def get_network_metadata(network_id):
+    # TODO consider slicing only part of the metadata (id, name, desc. etc.)
+    return mangal_request("network", None, network_id, False)
+
+
+def get_network_vertices(network_id):
+    vertex_json = mangal_request("node", "network_id", network_id)
+    return [{"id": v['id'], "genus": v['original_name'].split(' ')[0],
+             "species": v['original_name'].split(' ')[1]} for v in vertex_json]
+
+
+def get_network_edges(network_id):
+    edge_json = mangal_request("interaction", "network_id", network_id)
+    edge_dict = dict()
+    for e in edge_json:
+        if (e['node_from'], e['node_to']) in edge_dict:
+            raise AssertionError("Multiple edges from {} to {}".format(
+                e['node_from'], e['node_to']))
+        if e['value'] == 0:
+            raise AssertionError("Edge from {} to {} has value 0".format(
+                e['node_from'], e['node_to']))
+
+        edge_dict[(e['node_from'], e['node_to'])] = e['value']
+    return edge_dict
+
+
+def fill_table_metadata(sheet, metadata):
+    sheet['A1'] = str(metadata)
+    sheet['C1'] = "plant_ge"
+    sheet['C2'] = "plant_sp"
+    sheet['C3'] = "no."
+    sheet['A3'] = "pol_ge"
+    sheet['B3'] = "pol_sp"
+
+
+def fill_table_vertices(sheet, vertex_list):
+    sheet_index = 4  # note that ws.cell function is 1 indexed
+    for v in vertex_list:
+        sheet.cell(row=sheet_index, column=1, value=v['genus'])
+        sheet.cell(row=sheet_index, column=2, value=v['species'])
+        sheet.cell(row=sheet_index, column=3, value=v['id'])
+
+        sheet.cell(row=1, column=sheet_index, value=v['genus'])
+        sheet.cell(row=2, column=sheet_index, value=v['species'])
+        sheet.cell(row=3, column=sheet_index, value=v['id'])
+
+        sheet_index += 1
+
+
+def fill_table_edges(sheet, edge_dict, row_length):
+    for i in range(4, 4 + row_length):
+        for j in range(4, 4 + row_length):
+            pair_id = sheet.cell(row=i, column=3).value, sheet.cell(row=3, column=j).value
+            sheet.cell(row=i, column=j, value=edge_dict.get(pair_id, 0))
+
+
+def construct_network_excel(network_id, save_path=SAVE_PATH):
+    try:
+        network_metadata = get_network_metadata(network_id)
+        network_vertices = get_network_vertices(network_id)
+        network_edges = get_network_edges(network_id)
+    except AssertionError:
+        print("Error parsing network ", network_id)
+        raise
+
+    network_book = openpyxl.Workbook()
+    network_sheet = network_book.active
+
+    fill_table_metadata(network_sheet, network_metadata)
+    fill_table_vertices(network_sheet, network_vertices)
+    fill_table_edges(network_sheet, network_edges, len(network_vertices))
+    network_book.save(save_path.format(filename=network_metadata['name']))
+
+
+def get_network_iterator():
+    page = 0
+    network_page = mangal_request("network", "page", page)
+    while network_page:
+        for net in network_page:
+            yield net
+        page += 1
+        network_page = mangal_request("network", "page", page)
+
+
+if __name__ == "__main__":
+    acc = 0
+    for net in get_network_iterator():
+        if "poll" in net['description']:
+            construct_network_excel(net['id'])
+            acc += 1
+    print(acc)
---
+++ taxonomy_resolution.py
@@ -0,0 +1,41 @@
+import requests as req
+
+"""
+    Taxonomy resolution using the GBIF backbone database (https://www.gbif.org/), used to
+    order mangal data as a true bipartite network in the excel file.
+
+    This is done by getting the kingdom of a given species: if it's from Plantae, it's a 
+    plant, if it's from Animalia, it's a pollinator.
+    
+    Note that GBIF requests only work for either a 'genus' query or a 'species' query of
+    two words (genus species).
+"""
+
+GBIF_URL = "https://api.gbif.org/v1/species/match?rank={rank}&strict=false&name={species_name}"
+
+
+def gbif_request(search_value, delimeter='_', only_genus=False):
+    request = GBIF_URL.format(rank=("genus" if only_genus else "species"),
+                              species_name=delimeter.join(search_value.split(' ')))
+    response = req.get(request)
+
+    if response.status_code != 200:
+        raise ConnectionError("request '{0}' failed with error code {1}.".format(request, response.status_code))
+    return response.json()
+
+
+def get_kingdom(name):
+    if len(name.split(' ')) < 2:
+        taxonomy = gbif_request(name, only_genus=True)
+    elif len(name.split(' ')) == 2:
+        taxonomy = gbif_request(name)
+    else:
+        raise ValueError("Request taxonomy only with 'species' or 'genus species'")
+
+    if 'kingdom' not in taxonomy.keys():
+        raise KeyError("{} kingdom not found, with response: {}".format(name, taxonomy))
+    return taxonomy['kingdom']
+
+
+if __name__ == "__main__":
+    assert get_kingdom("Echium wildpretii") == get_kingdom("Echium") == "Plantae"
