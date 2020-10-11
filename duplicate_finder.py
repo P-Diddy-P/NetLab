df4a8e3d4ea019069066a39572da3989876638b1
Omer Shapira <omer.sh10@gmail.com>
2020-10-11 11:23:12 +0300

    Removed old attempt at duplicate node names. Rethink duplicate finding process.

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -31,70 +31,16 @@ def extract_networks(source_directories):
     return all_networks
 
 
-def similar_pattern(s1, s2):
-    raise NotImplementedError
-
-
-def get_similar_nodes(nodes1, nodes2):
-    similar_pairs = set()
-    tagged1, tagged2 = set(), set()
-
-    for n1 in nodes1:
-        if n1 in tagged1:
-            continue
-
-        for n2 in nodes2:
-            if n2 in tagged2:
-                continue
-
-            if similar_pattern(n1, n2):
-                similar_pairs.add((n1, n2))
-                tagged1.add(n1)
-                tagged2.add(n2)
-                break
-
-    return similar_pairs
-
-
-def compare_nodes(nodes1, nodes2, fraction=0.05):
-    mean_measure = ((len(nodes1) + len(nodes2)) / 2) * fraction
-    print(f'mean measure of both networks by fraction {fraction} is {mean_measure}')
-    print(f'node diff is {abs(len(nodes1) - len(nodes2))}, and '
-          f'{abs(len(nodes1) - len(nodes2)) / mean_measure} in mean measure')
-
-    similar = set(nodes1).intersection(set(nodes2))
-    print(f'{len(similar)} similar nodes:\n{similar}')
-    print(f'{len(set(nodes1) - similar)} non-similars in net1:\n{sorted(list(set(nodes1) - similar))}')
-    print(f'{len(set(nodes2) - similar)} non-similars in net1:\n{sorted(list(set(nodes2) - similar))}')
-
-
-def special_pattern(string):
-    special_chars = r'[^a-zA-Z]'
-    for word in string.split(' '):
-        if re.search(special_chars, word):
-            return True
-    return False
-
-
 if __name__ == "__main__":
     # TODO currently doesn't parse IWDB networks, as they have no set format.
     # TODO add IWDB (argv[1]) once it's directory is in order.
-    # networks = extract_networks([pathlib.Path(s) for s in argv[2:]])
-    sources = [pathlib.Path(argv[2])]
+    networks = extract_networks([pathlib.Path(s) for s in argv[2:]])
+    network_keys = list(networks.keys())
 
-    for net_name, network in extract_networks(sources).items():
-        plant_sp = 0
-        pol_sp = 0
-        print(f"============================================\n"
-              f"searching {net_name}\n")
-        for plant_name in network.index:
-            if special_pattern(plant_name):
-                plant_sp += 1
-                print(plant_name)
+    for i in range(len(network_keys)):
+        refnet = networks[network_keys[i]]
+        refplants = refnet.index  # add preprocessing for vertex lists
+        refpols = refnet.columns
 
-        for pol_name in network.columns:
-            if special_pattern(pol_name):
-                pol_sp += 1
-                print(pol_name)
-        print(f"\ntotal {plant_sp} special plants || {pol_sp} special pollinators")
-        print("============================================\n")
+        for j in range(i + 1, len(network_keys)):
+            pass
